1294ec81e9d2e0d9816287bec041f0932ecdf040
Sean Newman <seangnewman@msn.com>
2018-09-18 06:22:47 -0700

    Beginning discussion on Dynamic Loading

---
+++ Library/Implementations.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class StandardCatalog : ISaveable, IPersistable
+    {
+        public void Load()
+        {
+
+        }
+
+        public string Save()
+        {
+            return "Catalog Save";
+        }
+    }
+
+    public class ExplicitCatalog : ISaveable, IPersistable
+    {
+        public string Save()
+        {
+            return "Catalog Save";
+        }
+        string ISaveable.Save()
+        {
+            return "ISaveable Save";
+        }
+
+        string IPersistable.Save()
+        {
+            return "IPersistable Save";
+        }
+    }
+
+    public class Catalog : ISaveable, IVoidSaveable
+    {
+        public string Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IVoidSaveable.Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class EnumerableCatalog : IEnumerable<string>
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            // Custom code
+            return null;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+
+}
---
+++ OutputViewer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+
+namespace OutputViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StandardCatalog standardCatalog = new StandardCatalog();
+            ISaveable saveable = new StandardCatalog();
+            IPersistable persistable = new StandardCatalog();
+
+            Console.WriteLine("Standard Implementation");
+            Console.WriteLine("Concrete Class:  {0}", standardCatalog.Save());
+            Console.WriteLine("ISaveable:       {0}", saveable.Save());
+            Console.WriteLine("IPersistable:    {0}", persistable.Save());
+            Console.WriteLine();
+
+            Console.WriteLine("Concrete Class:           {0}", standardCatalog.Save());
+            Console.WriteLine("(ISaveable)Catalog:       {0}", ((ISaveable)saveable).Save());
+            Console.WriteLine("(IPersistable)Catalog:    {0}", ((IPersistable)persistable).Save());
+            Console.WriteLine();
+
+            Console.WriteLine("==========================================================");
+
+            ExplicitCatalog explicitCatalog = new ExplicitCatalog();
+            saveable = new ExplicitCatalog();
+            persistable = new ExplicitCatalog();
+
+            Console.WriteLine("Explicit Implementation");
+            Console.WriteLine("Concrete Class:  {0}", explicitCatalog.Save());
+            Console.WriteLine("ISaveable:       {0}", saveable.Save());
+            Console.WriteLine("IPersistable:    {0}", persistable.Save());
+            Console.WriteLine();
+
+            Console.WriteLine("Concrete Class:           {0}", explicitCatalog.Save());
+            Console.WriteLine("(ISaveable)Catalog:       {0}", ((ISaveable)explicitCatalog).Save());
+            Console.WriteLine("(IPersistable)Catalog:    {0}", ((IPersistable)explicitCatalog).Save());
+            Console.WriteLine();
+        }
+    }
+}
